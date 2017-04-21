8a74f7389c983af6f01fb5f0fd141d8ad2810588
siddharth-daftari <siddharth.daftari@gmail.com>
2017-04-20 19:30:33 -0700

    initial commit

---
+++ src/main/java/edu/sjsu/cmpe275/AirlineReservationSystemApplication.java
@@ -0,0 +1,12 @@
+package edu.sjsu.cmpe275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationSystemApplication.class, args);
+	}
+}
